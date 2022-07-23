ac05b66dc24266ceefde7c51ed883d8e2091079d
Alex Wolf <f.alexander.wolf@gmail.com>
2022-07-23 08:43:42 +0200

    🎉 Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# lndb-schema-core: Core schema module
+
+This package is in private beta at this moment!
+
+Follow https://twitter.com/laminlabs to learn about a first public release.
+
+For beta users: Read the [docs](https://lamin.ai/docs/lndb-schema-core).
---
+++ lndb_schema_core/__init__.py
@@ -0,0 +1,18 @@
+"""Core schema module.
+
+Import the package::
+
+   import lndb_schema_core
+
+This is the complete API reference:
+
+.. autosummary::
+   :toctree: .
+
+   example_function
+   ExampleClass
+"""
+
+__version__ = "0.0.1"  # denote a pre-release for 0.1.0 with 0.1a1
+
+from ._core import ExampleClass, example_function  # noqa
