447d243802c160f0896bd0aa1b0bd9c582324087
Alex Wolf <f.alexander.wolf@gmail.com>
2022-07-31 22:11:13 +0200

    :memo: Fix docs

--- lndb_schema_core/__init__.py
+++ lndb_schema_core/__init__.py
@@ -1,4 +1,25 @@
-"""Core schema module."""
+"""Core schema module.
+
+Import the package::
+
+   import lndb_schema_core
+
+This is the complete API reference:
+
+.. autosummary::
+   :toctree: .
+
+   dobject
+   dtransform
+   dtransform_in
+   dtransform_out
+   jupynb
+   lndb_schema_core
+   track_do
+   track_do_type
+   user
+
+"""
 
 __version__ = "0.2.0"  # denote a pre-release for 0.1.0 with 0.1a1
 
