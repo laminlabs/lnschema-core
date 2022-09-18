610522a2a80252b9921fafa4a8bd8d96ad26e00a
Alex Wolf <f.alexander.wolf@gmail.com>
2022-09-18 18:19:07 +0200

    :bookmark: Release version 0.7.1

--- lnschema_core/__init__.py
+++ lnschema_core/__init__.py
@@ -43,8 +43,8 @@ Auxiliary modules:
 """
 # This is lnschema-module yvzi.
 _schema_id = "yvzi"
-_migration = "3b60b87450c0"
-__version__ = "0.7.0"  # denote a pre-release for 0.1.0 with 0.1a1
+_migration = "049d7dfc80a8"
+__version__ = "0.7.1"  # denote a pre-release for 0.1.0 with 0.1a1
 
 from . import id, type  # noqa
 from ._core import (  # noqa
