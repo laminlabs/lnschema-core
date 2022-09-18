9e3f16fb459dfa3e32baabcece9ba78be31d719b
Alex Wolf <f.alexander.wolf@gmail.com>
2022-09-18 17:29:12 +0200

    :truck: Rename file_suffix to suffix

--- lnschema_core/_core.py
+++ lnschema_core/_core.py
@@ -81,7 +81,7 @@ class dobject(SQLModel, table=True):  # type: ignore
     id: Optional[str] = Field(default_factory=idg.dobject, primary_key=True)
     v: Optional[str] = Field(default="1", primary_key=True)
     name: Optional[str] = Field(index=True)
-    file_suffix: str = Field(index=True)
+    suffix: str = Field(index=True)
     dtransform_id: str = Field(foreign_key="dtransform.id", index=True)
     storage_id: str = Field(foreign_key="storage.id", index=True)
     created_at: Optional[datetime] = CreatedAt
