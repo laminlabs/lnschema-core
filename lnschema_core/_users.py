c55f6ca539915aba1a8fc0ab2f29d257b185f455
Alex Wolf <f.alexander.wolf@gmail.com>
2022-10-07 13:12:17 +0200

    🚸 Auto-populated CreatedBy (#40)
    * :sparkles: Add auto-generated CreatedBy
    
    * :children_crossing: CreatedBy in a few tables
    
    * :bento: Added migration
    
    * :green_heart: Add lndb_setup to test dependencies

---
+++ lnschema_core/_users.py
@@ -0,0 +1,10 @@
+from sqlmodel import Field
+
+
+def current_user_id():
+    from lndb_setup import settings
+
+    return settings.user.id
+
+
+CreatedBy = Field(default_factory=current_user_id, foreign_key="user.id", index=True)
---
+++ lnschema_core/migrations/versions/2022-10-07-0c819d33ca9b-v0_10_0.py
@@ -0,0 +1,50 @@
+"""v0.10.0.
+
+Revision ID: 0c819d33ca9b
+Revises: 439c4ee0a22a
+Create Date: 2022-10-07 12:47:10.583291
+
+"""
+import sqlalchemy as sa  # noqa
+import sqlmodel  # noqa
+from alembic import op
+
+# revision identifiers, used by Alembic.
+revision = "0c819d33ca9b"
+down_revision = "439c4ee0a22a"
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    with op.batch_alter_table("jupynb", schema=None) as batch_op:
+        batch_op.add_column(sa.Column("created_by", sqlmodel.sql.sqltypes.AutoString()))
+        batch_op.drop_index("ix_jupynb_user_id")
+        batch_op.create_index(
+            batch_op.f("ix_jupynb_created_by"), ["created_by"], unique=False
+        )
+        batch_op.create_foreign_key("fk_created_by", "user", ["created_by"], ["id"])
+        batch_op.drop_column("user_id")
+
+    with op.batch_alter_table("pipeline", schema=None) as batch_op:
+        batch_op.add_column(sa.Column("created_by", sqlmodel.sql.sqltypes.AutoString()))
+        batch_op.create_index(
+            batch_op.f("ix_pipeline_created_by"), ["created_by"], unique=False
+        )
+        batch_op.create_foreign_key("fk_created_by", "user", ["created_by"], ["id"])
+
+    with op.batch_alter_table("pipeline_run", schema=None) as batch_op:
+        batch_op.add_column(sa.Column("created_by", sqlmodel.sql.sqltypes.AutoString()))
+        batch_op.create_index(
+            batch_op.f("ix_pipeline_run_created_by"), ["created_by"], unique=False
+        )
+        batch_op.create_foreign_key("fk_created_by", "user", ["created_by"], ["id"])
+
+    with op.batch_alter_table("version_yvzi", schema=None) as batch_op:
+        batch_op.create_index(
+            batch_op.f("ix_version_yvzi_user_id"), ["user_id"], unique=False
+        )
+
+
+def downgrade() -> None:
+    pass
