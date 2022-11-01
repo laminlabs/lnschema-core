d480f35e22aba591d4dbe939f361ad68fae80830
Alex Wolf <f.alexander.wolf@gmail.com>
2022-11-01 22:44:06 +0100

    :bento: Draft migration for postgres

--- lnschema_core/migrations/versions/2022-10-31-98da12fc80a8-v0_15_0.py
+++ lnschema_core/migrations/versions/2022-10-31-98da12fc80a8-v0_15_0.py
@@ -39,7 +39,31 @@ def upgrade() -> None:
             old_table_name="dset_dobject", new_table_name="core.dset_dobject"
         )
     else:
-        pass
+        op.rename_table(
+            old_table_name="public.dtransform", new_table_name="core.dtransform"
+        )
+        op.rename_table(old_table_name="public.storage", new_table_name="core.storage")
+        op.rename_table(old_table_name="public.user", new_table_name="core.user")
+        op.rename_table(
+            old_table_name="public.dtransform_in", new_table_name="core.dtransform_in"
+        )
+        op.rename_table(old_table_name="public.dobject", new_table_name="core.dobject")
+        op.rename_table(old_table_name="public.usage", new_table_name="core.usage")
+        op.rename_table(old_table_name="public.jupynb", new_table_name="core.jupynb")
+        op.rename_table(
+            old_table_name="public.pipeline", new_table_name="core.pipeline"
+        )
+        op.rename_table(
+            old_table_name="public.pipeline_run", new_table_name="core.pipeline_run"
+        )
+        op.rename_table(old_table_name="public.dset", new_table_name="core.dset")
+        op.rename_table(old_table_name="public.project", new_table_name="core.project")
+        op.rename_table(
+            old_table_name="public.project_dset", new_table_name="core.project_dset"
+        )
+        op.rename_table(
+            old_table_name="public.dset_dobject", new_table_name="core.dset_dobject"
+        )
 
 
 def downgrade() -> None:
