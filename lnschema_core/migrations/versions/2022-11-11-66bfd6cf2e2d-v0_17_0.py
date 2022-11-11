588e32f4c61732be9db80c642330ed93404fbaf3
Alex Wolf <f.alexander.wolf@gmail.com>
2022-11-11 22:49:00 +0100

    :construction: Iterate

--- lnschema_core/migrations/versions/2022-11-11-66bfd6cf2e2d-v0_17_0.py
+++ lnschema_core/migrations/versions/2022-11-11-66bfd6cf2e2d-v0_17_0.py
@@ -17,6 +17,7 @@ depends_on = None
 
 
 def upgrade() -> None:
+    op.execute("PRAGMA foreign_keys=OFF")
     with op.batch_alter_table("core.dtransform") as batch_op:
         batch_op.add_column(
             sa.Column("name", sqlmodel.sql.sqltypes.AutoString(), nullable=True)
@@ -28,24 +29,70 @@ def upgrade() -> None:
             sa.Column("pipeline_v", sqlmodel.sql.sqltypes.AutoString(), nullable=True)
         )
         batch_op.create_index(
-            batch_op.f("ix_core.run_pipeline_id"), ["pipeline_id"], unique=False
+            batch_op.f("ix_core.dtransform_pipeline_id"), ["pipeline_id"], unique=False
         )
         batch_op.create_index(
-            batch_op.f("ix_core.run_pipeline_v"), ["pipeline_v"], unique=False
+            batch_op.f("ix_core.dtransform_pipeline_v"), ["pipeline_v"], unique=False
         )
         batch_op.create_foreign_key(
-            batch_op.f("fk_core.run_pipeline_id_pipeline"),
+            batch_op.f("fk_core.dtransform_pipeline_id_pipeline"),
             "core.pipeline",
             ["pipeline_id", "pipeline_v"],
             ["id", "v"],
         )
+        batch_op.add_column(
+            sa.Column(
+                "created_by",
+                sqlmodel.sql.sqltypes.AutoString(),
+                nullable=False,
+                default="9ypQ1yrW",
+            )
+        )
+        batch_op.add_column(
+            sa.Column(
+                "created_at",
+                sa.DateTime(),
+                server_default=sa.text("(CURRENT_TIMESTAMP)"),
+                nullable=False,
+            )
+        )
+        batch_op.create_index(
+            batch_op.f("ix_core.dtransform_created_at"), ["created_at"], unique=False
+        )
+        batch_op.create_index(
+            batch_op.f("ix_core.dtransform_created_by"), ["created_by"], unique=False
+        )
+        batch_op.create_index(
+            batch_op.f("ix_core.dtransform_name"), ["name"], unique=False
+        )
+        batch_op.create_foreign_key(
+            batch_op.f("fk_core.dtransform_created_by_user"),
+            "core.user",
+            ["created_by"],
+            ["id"],
+        )
 
+    op.execute(
+        """
+    update core.dtransform
+    set core.dtransform.created_by = core.run.created_by,
+    core.dtransform.created_at = core.dtransform.created_at,
+    core.dtransform.name = core.run.name,
+    core.dtransform.pipeline_id = core.run.pipeline_id,
+    core.dtransform.pipeline_v = core.run.pipeline_v,
+    from core.run
+    where core.dtransform.run_id = core.run.id
+    """
+    )
+
+    op.drop_table("core.run")
+
+    op.rename_table(old_table_name="core.dtransform", new_table_name="core.run")
     op.rename_table(old_table_name="core.dtransform_in", new_table_name="core.run_in")
     op.alter_column(
         "core.dobject", column_name="dtransform_id", new_column_name="run_id"
     )
-
-    op.drop_table("core.dtransform")
+    op.execute("PRAGMA foreign_keys=ON")
 
 
 def downgrade() -> None:
