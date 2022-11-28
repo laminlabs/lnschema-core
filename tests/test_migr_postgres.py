caf1a0ea305c8efd4a29ad4ddbd23d19aa95b56e
Alex Wolf <f.alexander.wolf@gmail.com>
2022-11-28 01:55:55 +0100

    :green_heart: Fix

--- tests/test_migr_postgres.py
+++ tests/test_migr_postgres.py
@@ -1,8 +1,8 @@
-from lndb_setup._test_migrate import test_migrate
+from lndb_setup._test_migrate import migrate_test
 
 
 def test_migrate_postgres():
-    results = test_migrate("lnschema_core", n_instances=1, dialect_name="postgresql")
+    results = migrate_test("lnschema_core", n_instances=1, dialect_name="postgresql")
     print(results)
     # if "migrate-failed" in results:
     #     raise RuntimeError("Migration failed.")
--- tests/test_migr_sqlite.py
+++ tests/test_migr_sqlite.py
@@ -1,7 +1,7 @@
-from lndb_setup._test_migrate import test_migrate
+from lndb_setup._test_migrate import migrate_test
 
 
 def test_migrate_sqlite():
-    results = test_migrate("lnschema_core", n_instances=1, dialect_name="sqlite")
+    results = migrate_test("lnschema_core", n_instances=1, dialect_name="sqlite")
     if "migrate-failed" in results:
         raise RuntimeError("Migration failed.")
