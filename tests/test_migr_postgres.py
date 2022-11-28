58070fc9e03ab40d8f9b92ff6fdd405a00e1ac9e
Alex Wolf <f.alexander.wolf@gmail.com>
2022-11-28 19:14:23 +0100

    :arrow_up: Upgrade lndb-setup

--- tests/test_migr_postgres.py
+++
@@ -1,8 +0,0 @@
-from lndb_setup._test_migrate import migrate_test
-
-
-def test_migrate_postgres():
-    results = migrate_test("lnschema_core", n_instances=1, dialect_name="postgresql")
-    print(results)
-    # if "migrate-failed" in results:
-    #     raise RuntimeError("Migration failed.")
--- tests/test_migrations.py
+++ tests/test_migrations.py
@@ -5,3 +5,10 @@ def test_migrate_sqlite():
     results = migrate_test("lnschema_core", n_instances=1, dialect_name="sqlite")
     if "migrate-failed" in results:
         raise RuntimeError("Migration failed.")
+
+
+def test_migrate_postgres():
+    results = migrate_test("lnschema_core", n_instances=1, dialect_name="postgresql")
+    print(results)
+    # if "migrate-failed" in results:
+    #     raise RuntimeError("Migration failed.")
