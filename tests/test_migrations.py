01745d47ed465113cd041d0760d868864587d567
Alex Wolf <f.alexander.wolf@gmail.com>
2022-12-08 09:52:47 +0100

    :recycle: Clean up

--- tests/test_migrations.py
+++ tests/test_migrations.py
@@ -5,13 +5,6 @@ with open("./lamin-project.yaml") as f:
     package_name = yaml.safe_load(f)["package_name"]
 
 
-# we can't run both the postgres and the sqlite test from the same process
-# right now as it seems to have a strange side effect on how indexes
-# are discovered, hence, we run this in the noxfile
-# def test_model_definitions_match_ddl_sqlite():
-#     model_definitions_match_ddl(package_name, dialect_name="sqlite")
-
-
 def test_model_definitions_match_ddl_postgres():
     model_definitions_match_ddl(package_name, dialect_name="postgres")
 
