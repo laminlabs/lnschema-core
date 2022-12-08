f00786d72aedbdc83ff985630a9602e0d1368130
Alex Wolf <f.alexander.wolf@gmail.com>
2022-12-08 11:58:43 +0100

    :recycle: More cleaning

--- noxfile.py
+++ noxfile.py
@@ -1,5 +1,5 @@
 import nox
-from lndb_setup.test._nox import (
+from lndb_setup.test.nox import (
     build_docs,
     login_testuser1,
     setup_test_instances_from_main_branch,
--- tests/test_migrations.py
+++ tests/test_migrations.py
@@ -1,6 +1,6 @@
 from lndb_setup.test import (
     get_package_name,
-    migrate_test_e2e,
+    migrate_clones,
     migration_id_is_consistent,
     model_definitions_match_ddl,
 )
@@ -16,13 +16,13 @@ def test_model_definitions_match_ddl_postgres():
     model_definitions_match_ddl(package_name, dialect_name="postgresql")
 
 
-def test_migrate_sqlite():
-    results = migrate_test_e2e(package_name, n_instances=1, dialect_name="sqlite")
+def test_migrate_clones_sqlite():
+    results = migrate_clones(package_name, n_instances=1, dialect_name="sqlite")
     if "migrate-failed" in results:
         raise RuntimeError("Migration e2e test failed.")
 
 
-# def test_migrate_postgres():
-#     results = migrate_test_e2e(package_name, n_instances=1, dialect_name="postgresql")
+# def test_migrate_clones_postgres():
+#     results = migrate_clones(package_name, n_instances=1, dialect_name="postgresql")
 #     if "migrate-failed" in results:
 #          raise RuntimeError("Migration e2e test failed.")
