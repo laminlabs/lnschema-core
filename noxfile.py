9f610825eb28010026c1323c6914906bfd5d8f23
Alex Wolf <f.alexander.wolf@gmail.com>
2022-12-08 12:23:12 +0100

    :lipstick: Prettify noxfile

--- noxfile.py
+++ noxfile.py
@@ -1,7 +1,9 @@
 import nox
 from lndb_setup.test.nox import (
     build_docs,
+    install_and_run_pytest,
     login_testuser1,
+    run_pre_commit,
     setup_test_instances_from_main_branch,
 )
 
@@ -10,22 +12,12 @@ nox.options.reuse_existing_virtualenvs = True
 
 @nox.session(python=["3.7", "3.8", "3.9", "3.10", "3.11"])
 def lint(session: nox.Session) -> None:
-    session.install("pre-commit")
-    session.run("pre-commit", "install")
-    session.run("pre-commit", "run", "--all-files")
+    run_pre_commit(session)
 
 
 @nox.session(python=["3.7", "3.8", "3.9", "3.10", "3.11"])
 def build(session):
     login_testuser1(session)
     setup_test_instances_from_main_branch(session)
-    session.install(".[dev,test]")
-    session.run(
-        "pytest",
-        "-s",
-        "--cov=lnschema_core",
-        "--cov-append",
-        "--cov-report=term-missing",
-    )
-    session.run("coverage", "xml")
+    install_and_run_pytest(session)
     build_docs(session)
