0060a0c6789d091a9f9e4bbd35a28f45ae2ab168
Bernardo Penteado <bernardopbf3@gmail.com>
2023-01-23 21:12:37 +0000

    :bookmark: Release version 0.25.1

--- lnschema_core/__init__.py
+++ lnschema_core/__init__.py
@@ -36,7 +36,7 @@ Development tools:
 _schema_id = "yvzi"
 _name = "core"
 _migration = "f6b6b85cdffc"
-__version__ = "0.25.0"
+__version__ = "0.25.1"
 
 from . import dev, link
 from ._core import (
