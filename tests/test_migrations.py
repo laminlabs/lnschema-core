ff1feb994a5b3c562115d039ec0a2bef324d9abf
Sunny Sun <38218185+sunnyosun@users.noreply.github.com>
2023-02-14 14:43:31 +0800

    :test_tube: test

--- tests/test_migrations.py
+++ tests/test_migrations.py
@@ -20,12 +20,14 @@ def test_migration_id_is_consistent():
 
 def test_model_definitions_match_ddl_postgres():
     generate_module_files(package_name=package_name, migrations_path=migrations_path, schema_id=schema_id)
+    print("test_model_definitions_match_ddl_postgres env.py")
     print((Path(__file__).parent.parent / package_name / "migrations/env.py").exists())
     model_definitions_match_ddl(package_name, dialect_name="postgresql")
 
 
 def test_migrate_clones_sqlite():
     generate_module_files(package_name=package_name, migrations_path=migrations_path, schema_id=schema_id)
+    print("test_migrate_clones_sqlite env.py")
     print((Path(__file__).parent.parent / package_name / "migrations/env.py").exists())
     results = migrate_clones(package_name, n_instances=1, dialect_name="sqlite")
     if "migrate-failed" in results:
