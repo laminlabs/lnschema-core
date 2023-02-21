b70011fb62abddaaff8b2c6f5f1a5928546f22b6
Alex Wolf <f.alexander.wolf@gmail.com>
2023-02-21 23:01:24 +0100

    :memo: Fix

--- lnschema_core/migrations/versions/2023-02-21-1dafcf0b22aa-v0_28_0.py
+++ lnschema_core/migrations/versions/2023-02-21-1dafcf0b22aa-v0_28_0.py
@@ -17,6 +17,12 @@ def upgrade() -> None:
         prefix, schema = "", "core"
 
     op.alter_column(f"{prefix}dobject", column_name="run_id", new_column_name="source_id", schema=schema)
+    if sqlite:
+        op.drop_index("ix_core.dobject_run_id", table_name="dobject")
+        op.create_index(op.f("ix_core.dobject_source_id"), "dobject", ["source_id"], unique=False)
+    else:
+        op.drop_index("ix_core_dobject_run_id", table_name="dobject", schema="core")
+        op.create_index(op.f("ix_core_dobject_source_id"), "dobject", ["source_id"], unique=False, schema="core")
 
 
 def downgrade() -> None:
