3ca997c309d0e26504f660b3dbb60560e86a791d
Alex Wolf <f.alexander.wolf@gmail.com>
2023-02-21 22:34:31 +0100

    :green_heart: Fix linting

--- lnschema_core/migrations/versions/2023-02-21-1dafcf0b22aa-v0_28_0.py
+++ lnschema_core/migrations/versions/2023-02-21-1dafcf0b22aa-v0_28_0.py
@@ -1,10 +1,10 @@
 """v0.28.0."""
-from alembic import op
 import sqlalchemy as sa  # noqa
 import sqlmodel as sqm  # noqa
+from alembic import op
 
-revision = '1dafcf0b22aa'
-down_revision = '8280855a5064'
+revision = "1dafcf0b22aa"
+down_revision = "8280855a5064"
 
 
 def upgrade() -> None:
