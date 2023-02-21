eb96977bfac76feec14014ed03b6dde1bb32848e
Alex Wolf <f.alexander.wolf@gmail.com>
2023-02-21 23:42:41 +0100

    :green_heart: Fix

--- lnschema_core/migrations/versions/2023-02-21-1dafcf0b22aa-v0_28_0.py
+++ lnschema_core/migrations/versions/2023-02-21-1dafcf0b22aa-v0_28_0.py
@@ -21,7 +21,7 @@ def upgrade() -> None:
         with op.batch_alter_table("core.dobject", schema=None) as batch_op:
             try:
                 batch_op.drop_index("ix_core.dobject_run_id")
-            except ValueError:
+            except Exception:
                 pass
             batch_op.create_index(batch_op.f("ix_core.dobject_source_id"), ["source_id"], unique=False)
             batch_op.create_foreign_key(batch_op.f("fk_core.dobject_source_id_run"), "core.run", ["source_id"], ["id"])
