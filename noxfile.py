5bd1b1395d26ae504cbf990f4dd5a6d50d0cc572
Alex Wolf <f.alexander.wolf@gmail.com>
2023-02-22 21:46:27 +0100

    :green_heart: Fix

--- noxfile.py
+++ noxfile.py
@@ -1,6 +1,5 @@
 import os
 import shutil
-from pathlib import Path
 
 import lndb
 import nox
@@ -20,15 +19,15 @@ def upload_run():
     print(os.environ["GITHUB_EVENT_NAME"] == "push")
     print(os.environ["GITHUB_EVENT_NAME"] != "push")
     package_name = get_package_name()
-    filename = f"./{package_name}_docs.zip"
-    shutil.make_archive(filename, "zip", "./docs")
+    filestem = f"{package_name}_docs"
+    filename = shutil.make_archive(filestem, "zip", "./docs")
     lndb.load("testuser1/lamin-site-assets", migrate=True)
 
     import lamindb as ln
     import lamindb.schema as lns
 
     with ln.Session() as ss:
-        dobject = ss.select(ln.DObject, name=Path(filename).stem).one_or_none()
+        dobject = ss.select(ln.DObject, name=filestem).one_or_none()
         dobject_id = None if dobject is None else dobject.id
         pipeline = ln.add(lns.Pipeline, name=f"CI {package_name}")
         run = lns.Run(pipeline=pipeline)
@@ -44,6 +43,7 @@ def lint(session: nox.Session) -> None:
 @nox.session(python=["3.7", "3.8", "3.9", "3.10", "3.11"])
 def build(session):
     login_testuser1(session)
+    session.install("lamin_logger>=0.3rc1")
     # setup_test_instances_from_main_branch(session)
     # session.install(".[dev,test]")
     # run_pytest(session)
