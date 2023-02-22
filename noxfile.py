34bfe9144c21c44b7f9b09e4d4286c76c996b965
Alex Wolf <f.alexander.wolf@gmail.com>
2023-02-22 20:56:44 +0100

    :memo: Add upload step to CI

--- noxfile.py
+++ noxfile.py
@@ -1,4 +1,10 @@
+import shutil
+
+import lamindb as ln
+import lamindb.schema as lns
+import lndb
 import nox
+from lndb.test._env import get_package_name
 from lndb.test.nox import (
     build_docs,
     login_testuser1,
@@ -8,6 +14,20 @@ from lndb.test.nox import (
 )
 
 nox.options.reuse_existing_virtualenvs = True
+
+
+def upload_run(session):
+    package_name = get_package_name()
+    filename = f"{package_name}_docs.zip"
+    shutil.make_archive(filename, "zip", "./docs")
+    lndb.load("testuser1/lamin-site-assets", migrate=True)
+    with ln.Session() as ss:
+        dobject = ss.select(ln.DObject, name=filename).one_or_none()
+        dobject_id = None if dobject is None else dobject.id
+        pipeline = ln.add(lns.Pipeline, name=f"CI {package_name}")
+        run = lns.Run(pipeline=pipeline)
+        dobject = ln.DObject(filename, id=dobject_id, run=run)
+        ss.add(dobject)
 
 
 @nox.session(python=["3.7", "3.8", "3.9", "3.10", "3.11"])
