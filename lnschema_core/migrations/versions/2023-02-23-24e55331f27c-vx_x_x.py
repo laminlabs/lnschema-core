b521ae64b5d7d3f1394116239dbe223de4aac86f
Sunny Sun <38218185+sunnyosun@users.noreply.github.com>
2023-02-23 17:21:48 +0800

    :green_heart: fix mig

--- lnschema_core/migrations/versions/2023-02-23-24e55331f27c-vx_x_x.py
+++ lnschema_core/migrations/versions/2023-02-23-24e55331f27c-vx_x_x.py
@@ -29,6 +29,31 @@ def upgrade() -> None:
         schema=schema,
     )
 
+    if sqlite:
+        with op.batch_alter_table(f"{prefix}dobject", schema=schema) as batch_op:
+            batch_op.drop_index("ix_core.dobject__filekey")
+            batch_op.create_index(batch_op.f("ix_core.dobject__objectkey"), ["_objectkey"], unique=False)
+        with op.batch_alter_table(f"{prefix}dfolder", schema=schema) as batch_op:
+            batch_op.drop_index("ix_core.dfolder__folderkey")
+            batch_op.create_index(batch_op.f("ix_core.dfolder__objectkey"), ["_objectkey"], unique=False)
+    else:
+        op.drop_index("ix_core_dobject__filekey", table_name="dobject", schema="core")
+        op.create_index(
+            op.f("ix_core_dobject__objectkey"),
+            "dobject",
+            ["_objectkey"],
+            unique=False,
+            schema="core",
+        )
+        op.drop_index("ix_core_dfolder__folderkey", table_name="dfolder", schema="core")
+        op.create_index(
+            op.f("ix_core_dfolder__objectkey"),
+            "dfolder",
+            ["_objectkey"],
+            unique=False,
+            schema=schema,
+        )
+
 
 def downgrade() -> None:
     pass
