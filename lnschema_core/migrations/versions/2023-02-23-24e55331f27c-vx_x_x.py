24a685b81e1d4fb32537d9c2bf7fa675bd72428f
Sunny Sun <38218185+sunnyosun@users.noreply.github.com>
2023-02-23 17:36:45 +0800

    :green_heart: fix mig

--- lnschema_core/migrations/versions/2023-02-23-24e55331f27c-vx_x_x.py
+++ lnschema_core/migrations/versions/2023-02-23-24e55331f27c-vx_x_x.py
@@ -19,40 +19,33 @@ def upgrade() -> None:
     op.alter_column(
         f"{prefix}dobject",
         column_name="_filekey",
-        new_column_name="_dobjectkey",
+        new_column_name="_objectkey",
         schema=schema,
     )
     op.alter_column(
         f"{prefix}dfolder",
         column_name="_folderkey",
-        new_column_name="_dobjectkey",
+        new_column_name="_objectkey",
         schema=schema,
     )
 
     if sqlite:
-        with op.batch_alter_table(f"{prefix}dobject", schema=schema) as batch_op:
-            batch_op.drop_index("ix_core.dobject__filekey")
-            batch_op.create_index(batch_op.f("ix_core.dobject__objectkey"), ["_objectkey"], unique=False)
-        with op.batch_alter_table(f"{prefix}dfolder", schema=schema) as batch_op:
-            batch_op.drop_index("ix_core.dfolder__folderkey")
-            batch_op.create_index(batch_op.f("ix_core.dfolder__objectkey"), ["_objectkey"], unique=False)
+        drop_index_filekey = "ix_core.dobject__filekey"
+        drop_index_folderkey = "ix_core.dfolder__folderkey"
+        create_index_objectkey_dobject = "ix_core.dobject__objectkey"
+        create_index_objectkey_dfolder = "ix_core.dfolder__objectkey"
     else:
-        op.drop_index("ix_core_dobject__filekey", table_name="dobject", schema="core")
-        op.create_index(
-            op.f("ix_core_dobject__objectkey"),
-            "dobject",
-            ["_objectkey"],
-            unique=False,
-            schema="core",
-        )
-        op.drop_index("ix_core_dfolder__folderkey", table_name="dfolder", schema="core")
-        op.create_index(
-            op.f("ix_core_dfolder__objectkey"),
-            "dfolder",
-            ["_objectkey"],
-            unique=False,
-            schema="core",
-        )
+        drop_index_filekey = "ix_core_dobject__filekey"
+        drop_index_folderkey = "ix_core_dfolder__folderkey"
+        create_index_objectkey_dobject = "ix_core_dobject__objectkey"
+        create_index_objectkey_dfolder = "ix_core_dfolder__objectkey"
+
+        with op.batch_alter_table(f"{prefix}dobject", schema=schema) as batch_op:
+            batch_op.drop_index(drop_index_filekey)
+            batch_op.create_index(batch_op.f(create_index_objectkey_dobject), ["_objectkey"], unique=False)
+        with op.batch_alter_table(f"{prefix}dfolder", schema=schema) as batch_op:
+            batch_op.drop_index(drop_index_folderkey)
+            batch_op.create_index(batch_op.f(create_index_objectkey_dfolder), ["_objectkey"], unique=False)
 
 
 def downgrade() -> None:
