60285a96d2c37bd1141462304b9bf4cd4a4c3e0b
Alex Wolf <f.alexander.wolf@gmail.com>
2023-03-23 09:43:28 +0100

    :green_heart: Fix

--- lnschema_core/migrations/versions/2023-03-23-ebafd37fd6e1-v0_30_rc2.py
+++ lnschema_core/migrations/versions/2023-03-23-ebafd37fd6e1-v0_30_rc2.py
@@ -30,17 +30,40 @@ def upgrade() -> None:
     if sqlite:  # sqlite needs quotes
         core_notebook, core_pipeline, core_run = f"'{core_notebook}'", f"'{core_pipeline}'", f"'{core_run}'"
 
-    op.add_column(table_name=f"{prefix}notebook", column=sa.Column("type", sa.String), schema=schema)
+    # add the type column to the existing notebook table
+    op.alter_column(f"{prefix}notebook", "type", existing_type=sa.VARCHAR(), type_=sa.Enum("pipeline", "notebook", name="transformtype"), nullable=False, schema=schema)
+
     op.execute(f"update {core_notebook} set type = 'notebook'")
     op.execute(copy_pipeline_to_notebook.format(core_notebook=core_notebook, core_pipeline=core_pipeline))
     op.execute(f"update {core_notebook} set type = 'pipeline' where type is null")
     op.execute(make_pipeline_references_notebook_references_in_run.format(core_run=core_run))
-    if sqlite:
-        op.drop_index(index_name=f"ix_core{delim}run_pipeline_id")
-        op.drop_index(index_name=f"ix_core{delim}run_pipeline_v")
-    # op.drop_column(table_name=f"{prefix}run", column_name="pipeline_id", schema=schema)
-    # op.drop_column(table_name=f"{prefix}run", column_name="pipeline_v", schema=schema)
-    # op.drop_table(table_name=f"{prefix}pipeline", schema=schema)
+
+    op.drop_index(f"ix_core{delim}pipeline_created_at", table_name=f"{prefix}pipeline", schema=schema)
+    op.drop_index(f"ix_core{delim}pipeline_created_by", table_name=f"{prefix}pipeline", schema=schema)
+    op.drop_index(f"ix_core{delim}pipeline_name", table_name=f"{prefix}pipeline", schema=schema)
+    op.drop_index(f"ix_core{delim}pipeline_reference", table_name=f"{prefix}pipeline", schema=schema)
+    op.drop_index(f"ix_core{delim}pipeline_updated_at", table_name=f"{prefix}pipeline", schema=schema)
+
+    op.drop_index(f"ix_core{delim}run_pipeline_id", table_name=f"{prefix}run", schema=schema)
+    op.drop_index(f"ix_core{delim}run_pipeline_v", table_name=f"{prefix}run", schema=schema)
+    op.drop_index(f"ix_core{delim}run_notebook_id", table_name=f"{prefix}run", schema=schema)
+    op.drop_index(f"ix_core{delim}run_notebook_v", table_name=f"{prefix}run", schema=schema)
+
+    with op.batch_alter_table(f"{prefix}run", schema=schema) as batch_op:
+        batch_op.drop_constraint("fk_run_notebook_id_notebook", type_="foreignkey")
+        batch_op.drop_constraint("fk_run_pipeline_id_pipeline", type_="foreignkey")
+
+        batch_op.alter_column(column_name="notebook_id", new_column_name="transform_id")
+        batch_op.alter_column(column_name="notebook_v", new_column_name="transform_v")
+
+        batch_op.drop_column(table_name=f"{prefix}run", column_name="pipeline_id")
+        batch_op.drop_column(table_name=f"{prefix}run", column_name="pipeline_v")
+
+    op.create_foreign_key(
+        op.f("fk_run_transform_id_transform"), f"{prefix}run", f"{prefix}transform", ["transform_id", "transform_v"], ["id", "v"], source_schema=schema, referent_schema=schema
+    )
+
+    op.drop_table(table_name=f"{prefix}pipeline", schema=schema)
     op.rename_table(old_table_name=f"{prefix}notebook", new_table_name=f"{prefix}transform", schema=schema)
 
 
