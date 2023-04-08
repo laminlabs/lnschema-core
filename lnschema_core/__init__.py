a617f4f13624d8605c31c07c1c170940e6a37b75
Alex Wolf <f.alexander.wolf@gmail.com>
2023-04-08 12:23:54 +0200

    :children_crossing: Strict erroring upon lack of instance setup

--- lnschema_core/__init__.py
+++ lnschema_core/__init__.py
@@ -4,9 +4,14 @@ _name = "core"
 _migration = "6de59093e378"
 __version__ = "0.31.0"
 
-from . import dev, link
-from ._core import Features, File, Folder, Project, Run, Storage, Transform, User
+from lndb._check_instance_setup import check_instance_setup as _check_instance_setup
 
-# backward compat
-DObject = File
-DFolder = Folder
+_INSTANCE_SETUP = _check_instance_setup()
+
+if _INSTANCE_SETUP:
+    from . import dev, link
+    from ._core import Features, File, Folder, Project, Run, Storage, Transform, User
+
+    # backward compat
+    DObject = File
+    DFolder = Folder
