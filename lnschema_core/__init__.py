0858c1addff223df569a75bf8ed4734586b0bc25
Alex Wolf <f.alexander.wolf@gmail.com>
2023-04-09 00:31:00 +0200

    :bookmark: Release version 0.32.0

--- lnschema_core/__init__.py
+++ lnschema_core/__init__.py
@@ -2,7 +2,7 @@
 _schema_id = "yvzi"
 _name = "core"
 _migration = "6de59093e378"
-__version__ = "0.31.0"
+__version__ = "0.32.0"
 
 from lndb._check_instance_setup import check_instance_setup as _check_instance_setup
 
