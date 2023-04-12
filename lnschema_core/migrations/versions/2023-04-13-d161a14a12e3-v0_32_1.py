64f94186dec91f7a3dea3057a3b3bf6dbdf20554
Alex Wolf <f.alexander.wolf@gmail.com>
2023-04-13 00:18:07 +0200

    :green_heart: Add migration

--- lnschema_core/migrations/versions/2023-04-13-d161a14a12e3-v0_32_1.py
+++ lnschema_core/migrations/versions/2023-04-13-d161a14a12e3-v0_32_1.py
@@ -16,7 +16,7 @@ SATransformType = sa.Enum(TransformType)
 # see https://medium.com/makimo-tech-blog/upgrading-postgresqls-enum-type-with-sqlalchemy-using-alembic-migration-881af1e30abe
 def upgrade() -> None:
     sqlite, prefix, schema, delim = get_sqlite_prefix_schema_delim_from_alembic()
-    if sqlite:
+    if not sqlite:
         with op.get_context().autocommit_block():
             op.execute("ALTER TYPE status ADD VALUE 'app'")
 
