ec12ce1dcca65423b95e69f827c310fc8fe0d07f
Alex Wolf <f.alexander.wolf@gmail.com>
2023-04-13 00:17:03 +0200

    :truck: Add migration

--- lnschema_core/__init__.py
+++ lnschema_core/__init__.py
@@ -1,7 +1,7 @@
 """Data lineage (`yvzi`)."""
 _schema_id = "yvzi"
 _name = "core"
-_migration = "6de59093e378"
+_migration = "d161a14a12e3"
 __version__ = "0.32.0"
 
 from lndb._check_instance_setup import check_instance_setup as _check_instance_setup
