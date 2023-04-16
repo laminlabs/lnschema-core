6be29844d1451da5267936cbad8187be7d01d081
Alex Wolf <f.alexander.wolf@gmail.com>
2023-04-16 16:39:41 +0200

    :green_heart: Fix 2

--- lnschema_core/migrations/versions/2023-04-16-6a73c00555b4-v0_33_0.py
+++ lnschema_core/migrations/versions/2023-04-16-6a73c00555b4-v0_33_0.py
@@ -13,48 +13,48 @@ def upgrade() -> None:
     sqlite, prefix, schema, delim = get_sqlite_prefix_schema_delim_from_alembic()
 
     op.alter_column(f"{prefix}file", column_name="source_id", new_column_name="run_id", schema=schema)
-    op.drop_index(f"ix_core{delim}file_source_id", table_name="file", schema=schema)
-    op.create_index(op.f(f"ix_core{delim}file_run_id"), "file", ["run_id"], unique=False, schema=schema)
+    op.drop_index(f"ix_core{delim}file_source_id", table_name=f"{prefix}file", schema=schema)
+    op.create_index(op.f(f"ix_core{delim}file_run_id"), f"{prefix}file", ["run_id"], unique=False, schema=schema)
 
     op.alter_column(f"{prefix}folder", column_name="created_by", new_column_name="created_by_id", schema=schema)
     op.alter_column(f"{prefix}run", column_name="created_by", new_column_name="created_by_id", schema=schema)
     op.alter_column(f"{prefix}features", column_name="created_by", new_column_name="created_by_id", schema=schema)
-    op.drop_index(f"ix_core{delim}features_created_by", table_name="features", schema="core")
-    op.create_index(op.f(f"ix_core{delim}features_created_by_id"), "features", ["created_by_id"], unique=False, schema=schema)
+    op.drop_index(f"ix_core{delim}features_created_by", table_name=f"{prefix}features", schema="core")
+    op.create_index(op.f(f"ix_core{delim}features_created_by_id"), f"{prefix}features", ["created_by_id"], unique=False, schema=schema)
 
     op.alter_column(f"{prefix}transform", column_name="created_by", new_column_name="created_by_id", schema=schema)
     op.alter_column(f"{prefix}project", column_name="created_by", new_column_name="created_by_id", schema=schema)
     op.add_column(f"{prefix}file", sa.Column("created_by_id", sqm.sql.sqltypes.AutoString(), nullable=True), schema=schema)
     op.add_column(f"{prefix}file", sa.Column("transform_id", sqm.sql.sqltypes.AutoString(), nullable=True), schema=schema)
     op.add_column(f"{prefix}file", sa.Column("transform_version", sqm.sql.sqltypes.AutoString(), nullable=True), schema=schema)
-    op.alter_column("file", "run_id", existing_type=sqm.sql.sqltypes.AutoString(), nullable=True, schema="core")
-    op.create_index(op.f(f"ix_core{delim}file_created_by_id"), "file", ["created_by_id"], unique=False, schema="core")
+    op.alter_column(f"{prefix}file", "run_id", existing_type=sqm.sql.sqltypes.AutoString(), nullable=True, schema="core")
+    op.create_index(op.f(f"ix_core{delim}file_created_by_id"), f"{prefix}file", ["created_by_id"], unique=False, schema="core")
     # op.add_column(f"{prefix}storage", sa.Column("created_by_id", sqm.sql.sqltypes.AutoString(), nullable=True), schema=schema)
 
     with op.batch_alter_table(f"{prefix}file", schema=schema) as batch_op:
         batch_op.create_foreign_key(op.f(f"fk_file{delim}created_by_id_user"), "user", ["created_by_id"], ["id"], referent_schema=schema)
 
-    op.drop_index(f"ix_core{delim}project_created_by", table_name="project", schema="core")
-    op.create_index(op.f(f"ix_core{delim}project_created_by_id"), "project", ["created_by_id"], unique=False, schema="core")
-    op.drop_index(f"ix_core{delim}run_created_by", table_name="run", schema="core")
-    op.create_index(op.f(f"ix_core{delim}run_created_by_id"), "run", ["created_by_id"], unique=False, schema="core")
-    op.drop_index(f"ix_core{delim}transform_created_by", table_name="transform", schema="core")
-    op.create_index(op.f(f"ix_core{delim}transform_created_by_id"), "transform", ["created_by_id"], unique=False, schema="core")
-    op.drop_index(f"ix_core{delim}folder_created_by", table_name="folder", schema="core")
-    op.create_index(op.f(f"ix_core{delim}folder_created_by_id"), "folder", ["created_by_id"], unique=False, schema="core")
-    op.create_index(op.f(f"ix_core{delim}file_transform_id"), "file", ["transform_id"], unique=False, schema=schema)
+    op.drop_index(f"ix_core{delim}project_created_by", table_name=f"{prefix}project", schema="core")
+    op.create_index(op.f(f"ix_core{delim}project_created_by_id"), f"{prefix}project", ["created_by_id"], unique=False, schema="core")
+    op.drop_index(f"ix_core{delim}run_created_by", table_name=f"{prefix}run", schema="core")
+    op.create_index(op.f(f"ix_core{delim}run_created_by_id"), f"{prefix}run", ["created_by_id"], unique=False, schema="core")
+    op.drop_index(f"ix_core{delim}transform_created_by", table_name=f"{prefix}transform", schema="core")
+    op.create_index(op.f(f"ix_core{delim}transform_created_by_id"), f"{prefix}transform", ["created_by_id"], unique=False, schema="core")
+    op.drop_index(f"ix_core{delim}folder_created_by", table_name=f"{prefix}folder", schema="core")
+    op.create_index(op.f(f"ix_core{delim}folder_created_by_id"), f"{prefix}folder", ["created_by_id"], unique=False, schema="core")
+    op.create_index(op.f(f"ix_core{delim}file_transform_id"), f"{prefix}file", ["transform_id"], unique=False, schema=schema)
 
     op.alter_column(f"{prefix}transform", column_name="v", new_column_name="version", schema=schema)
     op.alter_column(f"{prefix}run", column_name="transform_v", new_column_name="transform_version", schema=schema)
 
     with op.batch_alter_table(f"{prefix}file", schema=schema) as batch_op:
         batch_op.create_foreign_key(
-            op.f("fk_file_transform_id_version_transform"), "transform", ["transform_id", "transform_version"], ["id", "version"], referent_schema=schema
+            op.f("fk_file_transform_id_version_transform"), f"{prefix}transform", ["transform_id", "transform_version"], ["id", "version"], referent_schema=schema
         )
 
-    op.create_index(op.f(f"ix_core{delim}file_transform_version"), "file", ["transform_version"], unique=False, schema=schema)
-    op.drop_index(f"ix_core{delim}run_transform_v", "run", schema=schema)
-    op.create_index(op.f(f"ix_core{delim}run_transform_version"), "run", ["transform_version"], unique=False, schema=schema)
+    op.create_index(op.f(f"ix_core{delim}file_transform_version"), f"{prefix}file", ["transform_version"], unique=False, schema=schema)
+    op.drop_index(f"ix_core{delim}run_transform_v", f"{prefix}run", schema=schema)
+    op.create_index(op.f(f"ix_core{delim}run_transform_version"), f"{prefix}run", ["transform_version"], unique=False, schema=schema)
 
 
 def downgrade() -> None:
