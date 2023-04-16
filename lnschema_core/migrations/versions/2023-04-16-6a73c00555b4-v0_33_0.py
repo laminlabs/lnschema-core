4252564446fe292e8d9dd85e20b3eef21157538a
Alex Wolf <f.alexander.wolf@gmail.com>
2023-04-16 14:53:08 +0200

    :green_heart: Fix

--- lnschema_core/migrations/versions/2023-04-16-6a73c00555b4-v0_33_0.py
+++ lnschema_core/migrations/versions/2023-04-16-6a73c00555b4-v0_33_0.py
@@ -19,11 +19,26 @@ def upgrade() -> None:
     op.alter_column(f"{prefix}folder", column_name="created_by", new_column_name="created_by_id", schema=schema)
     op.alter_column(f"{prefix}run", column_name="created_by", new_column_name="created_by_id", schema=schema)
     op.alter_column(f"{prefix}features", column_name="created_by", new_column_name="created_by_id", schema=schema)
+    op.drop_index(f"ix_core{delim}features_created_by", table_name="features", schema="core")
+    op.create_index(op.f(f"ix_core{delim}features_created_by_id"), "features", ["created_by_id"], unique=False, schema=schema)
+
     op.alter_column(f"{prefix}transform", column_name="created_by", new_column_name="created_by_id", schema=schema)
     op.alter_column(f"{prefix}project", column_name="created_by", new_column_name="created_by_id", schema=schema)
     op.add_column(f"{prefix}file", sa.Column("created_by_id", sqm.sql.sqltypes.AutoString(), nullable=True), schema=schema)
     op.add_column(f"{prefix}file", sa.Column("transform_id", sqm.sql.sqltypes.AutoString(), nullable=True), schema=schema)
+    op.alter_column("file", "run_id", existing_type=sqm.sql.sqltypes.AutoString(), nullable=True, schema="core")
+    op.create_index(op.f(f"ix_core{delim}file_created_by_id"), "file", ["created_by_id"], unique=False, schema="core")
     # op.add_column(f"{prefix}storage", sa.Column("created_by_id", sqm.sql.sqltypes.AutoString(), nullable=True), schema=schema)
+
+    with op.batch_alter_table(f"{prefix}file", schema=schema):
+        op.create_foreign_key(op.f(f"fk_file{delim}created_by_id_user"), "user", ["created_by_id"], ["id"], referent_schema=schema)
+
+    op.drop_index(f"ix_core{delim}project_created_by", table_name="project", schema="core")
+    op.create_index(op.f(f"ix_core{delim}project_created_by_id"), "project", ["created_by_id"], unique=False, schema="core")
+    op.drop_index(f"ix_core{delim}run_created_by", table_name="run", schema="core")
+    op.create_index(op.f(f"ix_core{delim}run_created_by_id"), "run", ["created_by_id"], unique=False, schema="core")
+    op.drop_index(f"ix_core{delim}transform_created_by", table_name="transform", schema="core")
+    op.create_index(op.f(f"ix_core{delim}transform_created_by_id"), "transform", ["created_by_id"], unique=False, schema="core")
 
 
 def downgrade() -> None:
