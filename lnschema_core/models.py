e5de4954d3dc8179590a4ff50d4495d77bfb6086
Alex Wolf <f.alexander.wolf@gmail.com>
2023-05-30 13:43:15 +0200

    :recycle: Refactor models

--- lnschema_core/models.py
+++ lnschema_core/models.py
@@ -10,9 +10,11 @@ class RunInput(models.Model):
     run = models.ForeignKey("Run", on_delete=models.CASCADE)
     file = models.ForeignKey("File", on_delete=models.CASCADE)
 
+    class Meta:
+        managed = True
+
 
 class User(BaseORM):  # type: ignore
-    id = models.CharField(max_length=64, primary_key=True)
     email = models.CharField(max_length=64, unique=True)
     handle2 = models.CharField(max_length=64, unique=True)
     name = models.CharField(max_length=64, blank=True, null=True)
@@ -24,20 +26,18 @@ class User(BaseORM):  # type: ignore
 
 
 class Storage(BaseORM):  # type: ignore
-    id = models.CharField(max_length=64, primary_key=True)
     root = models.CharField(max_length=64)
     type = models.CharField(max_length=64, blank=True, null=True)
     region = models.CharField(max_length=64, blank=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-    created_by = models.ForeignKey("User", models.DO_NOTHING, blank=True, null=True, default=current_user_id)
+    created_by = models.ForeignKey(User, models.DO_NOTHING, blank=True, null=True, default=current_user_id)
 
     class Meta:
         managed = True
 
 
 class Project(BaseORM):  # type: ignore
-    id = models.CharField(max_length=64, primary_key=True)
     name = models.CharField(max_length=64)
     created_by = models.ForeignKey("User", models.DO_NOTHING, default=current_user_id)
     created_at = models.DateTimeField(auto_now_add=True)
@@ -48,9 +48,8 @@ class Project(BaseORM):  # type: ignore
 
 
 class Transform(models.Model):  # type: ignore
-    id = models.CharField(max_length=64, primary_key=True)
-    version = models.CharField(max_length=64)
     name = models.CharField(max_length=64)
+    version = models.CharField(max_length=64)
     type = models.CharField(max_length=64, choices=TransformType.choices(), db_index=True, default=(TransformType.notebook if is_run_from_ipython else TransformType.pipeline))
     title = models.CharField(max_length=64, blank=True, null=True)
     reference = models.CharField(max_length=64, blank=True, null=True)
@@ -60,39 +59,38 @@ class Transform(models.Model):  # type: ignore
 
     class Meta:
         managed = True
-        constraints = [models.UniqueConstraint(fields=["id", "version"], name="uq_transform_id_version")]
+        constraints = [models.UniqueConstraint(fields=["name", "version"])]
 
 
 class Run(models.Model):  # type: ignore
-    id = models.CharField(max_length=64, primary_key=True)
     name = models.CharField(max_length=64, blank=True, null=True)
     external_id = models.CharField(max_length=64, blank=True, null=True)
-    transform = models.ForeignKey("Transform", models.DO_NOTHING)
-    transform_version = models.CharField(max_length=64)
-    created_by = models.ForeignKey("User", models.DO_NOTHING)
-    created_at = models.DateTimeField(auto_now_add=True)
+    transform = models.ForeignKey(Transform, models.DO_NOTHING)
     inputs = models.ManyToManyField("File", through=RunInput, related_name="input_of")
+    # outputs on File
+    created_by = models.ForeignKey(User, models.DO_NOTHING)
+    created_at = models.DateTimeField(auto_now_add=True)
 
     class Meta:
         managed = True
 
 
 class Features(models.Model):  # type: ignore
-    id = models.CharField(max_length=64, primary_key=True)
     type = models.CharField(max_length=64)
-    created_by = models.ForeignKey("User", models.DO_NOTHING, default=current_user_id)
+    created_by = models.ForeignKey(User, models.DO_NOTHING, default=current_user_id)
     created_at = models.DateTimeField(auto_now=True)
+    files = models.ManyToManyField("File")
 
     class Meta:
         managed = True
 
 
 class Folder(models.Model):  # type: ignore
-    id = models.CharField(max_length=64, primary_key=True)
     name = models.CharField(max_length=64)
     key = models.CharField(max_length=64, blank=True, null=True)
-    storage = models.ForeignKey("Storage", models.DO_NOTHING)
-    created_by = models.ForeignKey("User", models.DO_NOTHING, default=current_user_id)
+    storage = models.ForeignKey(Storage, models.DO_NOTHING)
+    files = models.ManyToManyField("File")
+    created_by = models.ForeignKey(User, models.DO_NOTHING, default=current_user_id)
     created_at = models.DateTimeField()
     updated_at = models.DateTimeField(blank=True, null=True)
 
@@ -102,19 +100,20 @@ class Folder(models.Model):  # type: ignore
 
 
 class File(models.Model):  # type: ignore
-    id = models.CharField(max_length=64, primary_key=True)
-    size = models.BigIntegerField(blank=True, null=True)
     name = models.CharField(max_length=64, blank=True, null=True)
     suffix = models.CharField(max_length=64, blank=True, null=True)
+    size = models.BigIntegerField(blank=True, null=True)
     hash = models.CharField(max_length=64, blank=True, null=True)
     key = models.CharField(max_length=64, blank=True, null=True)
-    run = models.ForeignKey("Run", models.DO_NOTHING, blank=True, null=True)
-    transform = models.ForeignKey("Transform", models.DO_NOTHING, blank=True, null=True)
-    transform_version = models.CharField(max_length=64, blank=True, null=True)
-    storage = models.ForeignKey("Storage", models.DO_NOTHING)
+    run = models.ForeignKey(Run, models.DO_NOTHING, blank=True, null=True, related_name="outputs")
+    transform = models.ForeignKey(Transform, models.DO_NOTHING, blank=True, null=True)
+    storage = models.ForeignKey(Storage, models.DO_NOTHING)
+    # folders from Folders.files
+    # features from Features.files
+    # input_of from Run.inputs
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-    created_by = models.ForeignKey("User", models.DO_NOTHING, default=current_user_id)
+    created_by = models.ForeignKey(User, models.DO_NOTHING, default=current_user_id)
 
     class Meta:
         managed = True
