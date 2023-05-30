b519d8f1b731393e5c595e8508b8a84345fa3325
Alex Wolf <f.alexander.wolf@gmail.com>
2023-05-30 08:30:20 +0200

    :construction_worker: Refactor CI

--- tests/test_base.py
+++
@@ -1,8 +0,0 @@
-from lnschema_core.dev import id
-
-
-def test_id():
-    assert len(id.file()) == 20
-    assert len(id.user()) == 8
-    assert len(id.secret()) == 40
-    assert len(id.instance()) == 12
