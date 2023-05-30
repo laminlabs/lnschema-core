bc8f726b26117760f885b55c16efb58bfb715eec
Alex Wolf <f.alexander.wolf@gmail.com>
2023-05-30 21:27:36 +0200

    :recycle: Also load models for django

--- lnschema_core/__init__.py
+++ lnschema_core/__init__.py
@@ -4,8 +4,10 @@ _name = "core"
 _migration = "1c49c9f9a982"
 __version__ = "0.34.0"
 
-from lndb import _USE_DJANGO
 from lndb._check_instance_setup import check_instance_setup as _check_instance_setup
+
+import lndb as _lndb
+from lndb import _USE_DJANGO
 
 _INSTANCE_SETUP = _check_instance_setup()
 
@@ -13,7 +15,22 @@ if _INSTANCE_SETUP:
     from . import dev, link, types
 
     if not _USE_DJANGO:
-        from ._core import (
+        from lndb.dev._django import setup_django
+
+        setup_django(_lndb.settings.instance)
+
+        from .models import (
+            Features,
+            File,
+            Folder,
+            Project,
+            Run,
+            Storage,
+            Transform,
+            User,
+        )
+    else:
+        from ._core import (  # type: ignore
             Features,
             File,
             Folder,
