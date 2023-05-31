ca963cb8b59544122bd17dc3436ed2ff9527d201
Alex Wolf <f.alexander.wolf@gmail.com>
2023-05-31 21:23:25 +0200

    🏗️ Add Django backend (#175)
    * :recycle: Refactor models
    
    * :green_heart: Complete models
    
    * :building_construction: Add functional Django implementation
    
    * Clean up deps
    
    * :recycle: Also load models for django
    
    * :green_heart: Revert
    
    * :green_heart: Fix
    
    * :green_heart: Try to fix linting
    
    * :green_heart: Fix pyproject
    
    * :green_heart: Fix
    
    * :green_heart: Fix

--- lnschema_core/__init__.py
+++ lnschema_core/__init__.py
@@ -6,14 +6,31 @@ __version__ = "0.34.0"
 
 from lndb._check_instance_setup import check_instance_setup as _check_instance_setup
 
+import lndb as _lndb
+
+try:
+    from lndb import _USE_DJANGO
+except Exception:
+    _USE_DJANGO = False
+
 _INSTANCE_SETUP = _check_instance_setup()
-_USE_DJANGO: bool = False
 
 if _INSTANCE_SETUP:
     from . import dev, link, types
 
     if not _USE_DJANGO:
-        from ._core import (
+        from ._core import (  # type: ignore
+            Features,
+            File,
+            Folder,
+            Project,
+            Run,
+            Storage,
+            Transform,
+            User,
+        )
+    else:
+        from .models import (  # type: ignore
             Features,
             File,
             Folder,
--- lnschema_core/_users.py
+++ lnschema_core/_users.py
@@ -1,10 +1,16 @@
 from sqlmodel import Field
 
 
-def current_user_id():
+def current_user_id() -> str:
     from lndb import settings
 
     return settings.user.id
+
+
+def current_user_id_as_int() -> int:
+    from lndb import settings
+
+    return settings.user.get_id_as_int()
 
 
 CreatedBy = Field(default_factory=current_user_id, foreign_key="lnschema_core_user.id", index=True)
