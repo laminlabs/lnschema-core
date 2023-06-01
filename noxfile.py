af41a2696edcaaf228aebbeddac0a34407850da3
Alex Wolf <f.alexander.wolf@gmail.com>
2023-06-01 14:19:20 +0200

    :green_heart: Fix

--- noxfile.py
+++ noxfile.py
@@ -1,6 +1,7 @@
 import sys
 
 import nox
+import requests  # type: ignore
 from laminci import move_built_docs_to_docs_slash_project_slug, upload_docs_artifact
 from laminci.nox import build_docs, login_testuser1, run_pre_commit, run_pytest  # noqa
 
@@ -16,6 +17,9 @@ def lint(session: nox.Session) -> None:
 def install(session: nox.Session) -> None:
     session.run(*"pip install --no-deps .[django]".split())
     session.run(*"git clone https://github.com/laminlabs/lamindb --depth 1".split())
+    response = requests.get("https://github.com/laminlabs/lamindb/tree/rename")
+    if response.status_code < 400:
+        session.run(*"git switch rename".split())
     if sys.platform.startswith("linux"):  # remove version pin when running on CI
         session.run(*"sed -i /lnschema_core/d ./lamindb/pyproject.toml".split())
     session.run(*"pip install ./lamindb[aws,test]".split())
