75c1786ac3e044b0ff658b5688b38970054e44c7
Alex Wolf <f.alexander.wolf@gmail.com>
2023-06-04 13:48:00 +0200

    :green_heart: Prettify

--- lnschema_core/_lookup.py
+++ lnschema_core/_lookup.py
@@ -1,28 +1,30 @@
 import re
 from collections import namedtuple
-from typing import Any, Iterable, Optional
+from typing import Iterable, Optional
+
+from django.db import models
 
 
-def lookup(cls: Any, field: Optional[str] = None):
+def lookup(orm: models.Model, field: Optional[str] = None):
     """Lookup rows by field."""
     if field is None:
         # by default use the name field
-        if "name" in cls.__fields__:
+        if "name" in orm.__fields__:
             field = "name"
         else:
-            non_ids = [i for i in cls.__fields__.keys() if "id" not in i]
+            non_ids = [i for i in orm.__fields__.keys() if "id" not in i]
             if len(non_ids) > 0:
                 # the first field isn't named with id
                 field = non_ids[0]
             else:
                 # the first field
-                field = next(iter(cls.__fields__.keys()))
-    values = set(cls.objects.values_list(field, flat=True))
+                field = next(iter(orm.__fields__.keys()))
+    values = set(orm.objects.values_list(field, flat=True))
     for value in [None, ""]:
         if value in values:
             values.remove(value)
-    keys = to_lookup_keys(values, padding=cls.__name__)
-    nt = namedtuple_from_dict(d=dict(zip(keys, values)), name=cls.__name__)
+    keys = to_lookup_keys(values, padding=orm.__name__)
+    nt = namedtuple_from_dict(d=dict(zip(keys, values)), name=orm.__name__)
     return nt
 
 
