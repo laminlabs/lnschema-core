356f74293b0f49dc1b16cacf1a6e735d8c8d6bed
Alex Wolf <f.alexander.wolf@gmail.com>
2023-06-09 18:55:36 +0200

    :recycle: Create ChoicesMixin

--- lnschema_core/types.py
+++ lnschema_core/types.py
@@ -7,4 +7,28 @@
    DataLike
    TransformType
 """
-from ._types import DataLike, PathLike, TransformType  # noqa
+from enum import Enum
+from pathlib import Path
+from typing import Any, TypeVar
+
+from upath import UPath
+
+PathLike = TypeVar("PathLike", str, Path, UPath)
+# statically typing the following is hard because these are all heavy dependencies, even DataFrame is heavy & slow to import
+DataLike = Any
+
+
+class ChoicesMixin:
+    @classmethod
+    def choices(cls):
+        return [(item.value, item.name) for item in cls]
+
+
+class TransformType(ChoicesMixin, Enum):
+    pipeline = "pipeline"
+    notebook = "notebook"
+    app = "app"
+    api = "api"
+
+    def __repr__(self):
+        return self.name
