4ef04cb82d4460477c0316e0fab639eab9a72d28
Alex Wolf <f.alexander.wolf@gmail.com>
2023-06-12 07:39:30 +0200

    :fire: Remove file access helpers

--- lnschema_core/models.py
+++ lnschema_core/models.py
@@ -391,6 +391,8 @@ class Folder(BaseORM):
 
     def path(self) -> Union[Path, UPath]:
         """Path on storage."""
+        from lamindb._file_access import filepath_from_file_or_folder
+
         return filepath_from_file_or_folder(self)
 
     def tree(
@@ -498,6 +500,8 @@ class File(BaseORM):
 
     def path(self) -> Union[Path, UPath]:
         """Path on storage."""
+        from lamindb._file_access import filepath_from_file_or_folder
+
         return filepath_from_file_or_folder(self)
 
     # likely needs an arg `key`
@@ -648,6 +652,7 @@ class File(BaseORM):
         super().save(*args, **kwargs)
 
     def delete(self, *args, **kwargs) -> None:
+        from lamindb._file_access import storage_key_from_file
         from lamindb.dev.storage import delete_storage
 
         storage_key = storage_key_from_file(self)
@@ -670,42 +675,3 @@ class RunInput(BaseORM):
 
     class Meta:
         managed = True
-
-
-# add type annotations back asap when re-organizing the module
-def storage_key_from_file(file: File):
-    if file.key is None:
-        return f"{file.id}{file.suffix}"
-    else:
-        return file.key
-
-
-# add type annotations back asap when re-organizing the module
-def filepath_from_file_or_folder(file_or_folder: Union[File, Folder]):
-    from lamindb_setup import settings
-    from lamindb_setup.dev import StorageSettings
-
-    # using __name__ for type check to avoid need of
-    # dynamically importing the type
-    if file_or_folder.__name__ == "File":
-        storage_key = storage_key_from_file(file_or_folder)
-    else:
-        storage_key = file_or_folder.key
-        if storage_key is None:
-            raise ValueError("Only real folders have a path!")
-    if file_or_folder.storage_id == settings.storage.id:
-        path = settings.storage.key_to_filepath(storage_key)
-    else:
-        logger.warning(
-            "file.path() is slow for files outside the currently configured storage"
-            " location\nconsider joining for the set of files you're interested in:"
-            " ln.select(ln.File, ln.Storage)the path is storage.root / file.key if"
-            " file.key is not None\notherwise storage.root / (file.id + file.suffix)"
-        )
-        import lamindb as ln
-
-        storage = ln.select(ln.Storage, id=file_or_folder.storage_id).one()
-        # find a better way than passing None to instance_settings in the future!
-        storage_settings = StorageSettings(storage.root, instance_settings=None)
-        path = storage_settings.key_to_filepath(storage_key)
-    return path
