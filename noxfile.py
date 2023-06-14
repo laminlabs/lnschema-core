2d39e4670c42090ebfcd1c8ae958d68750283b73
Alex Wolf <f.alexander.wolf@gmail.com>
2023-06-14 19:15:08 +0200

    :white_check_mark: Add test

--- noxfile.py
+++ noxfile.py
@@ -1,5 +1,5 @@
 import nox
-from laminci.nox import run_pre_commit  # noqa
+from laminci.nox import run_pre_commit, run_pytest  # noqa
 
 nox.options.default_venv_backend = "none"
 
@@ -7,3 +7,8 @@ nox.options.default_venv_backend = "none"
 @nox.session
 def lint(session: nox.Session) -> None:
     run_pre_commit(session)
+
+
+@nox.session
+def test(session: nox.Session) -> None:
+    run_pytest(session)
