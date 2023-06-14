f81cd78decffb2348f85e7efab9b72ccdaef76c7
Alex Wolf <f.alexander.wolf@gmail.com>
2023-06-14 23:46:12 +0200

    ✅ Add migrations check (#202)
    * :white_check_mark: Add test
    
    * :white_check_mark: Fix
    
    * :green_heart: Fix
    
    * :green_heart: Fix order
    
    * :bento: Add lamin-project.yaml back
    
    * :green_heart: Fix
    
    * :green_heart: Fix

--- noxfile.py
+++ noxfile.py
@@ -1,5 +1,5 @@
 import nox
-from laminci.nox import run_pre_commit  # noqa
+from laminci.nox import login_testuser1, run_pre_commit, run_pytest  # noqa
 
 nox.options.default_venv_backend = "none"
 
@@ -7,3 +7,10 @@ nox.options.default_venv_backend = "none"
 @nox.session
 def lint(session: nox.Session) -> None:
     run_pre_commit(session)
+
+
+@nox.session
+def test(session: nox.Session) -> None:
+    session.run(*"pip install -e .[dev]".split())
+    login_testuser1(session)
+    run_pytest(session, coverage=False)
