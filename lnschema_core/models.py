9680cabbfe762b017f5765682fa1b93499bdb0fc
Alex Wolf <f.alexander.wolf@gmail.com>
2023-06-15 17:13:16 +0200

    🚚 Move transform methods

--- lnschema_core/models.py
+++ lnschema_core/models.py
@@ -6,7 +6,7 @@ from django.db.models import PROTECT, Manager
 
 from ._lookup import lookup as _lookup
 from ._queryset import QuerySet
-from .ids import base62, base62_8, base62_12, base62_20
+from .ids import base62_8, base62_12, base62_20
 from .types import TransformType
 from .users import current_user_id
 
@@ -192,23 +192,6 @@ class Transform(BaseORM):
 
     class Meta:
         unique_together = (("stem_id", "version"),)
-
-    def __init__(self, *args, **kwargs):
-        if len(args) > 0:  # initialize with all fields from db as args
-            super().__init__(*args, **kwargs)
-            return None
-        else:  # user-facing calling signature
-            # set default ids
-            if "id" not in kwargs and "stem_id" not in kwargs:
-                kwargs["id"] = base62(14)
-                kwargs["stem_id"] = kwargs["id"][:12]
-            elif "stem_id" in kwargs:
-                assert isinstance(kwargs["stem_id"], str) and len(kwargs["stem_id"]) == 12
-                kwargs["id"] = kwargs["stem_id"] + base62(2)
-            elif "id" in kwargs:
-                assert isinstance(kwargs["id"], str) and len(kwargs["id"]) == 14
-                kwargs["stem_id"] = kwargs["id"][:12]
-            super().__init__(**kwargs)
 
 
 class Run(BaseORM):
