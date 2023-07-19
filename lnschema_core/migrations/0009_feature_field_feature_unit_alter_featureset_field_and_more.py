fc6eb49cb7f1e0a480a6b2253b466d53af4f1343
Alex Wolf <f.alexander.wolf@gmail.com>
2023-07-19 13:58:14 +0200

    🚚 Move `feature_sets` to `File` instead of having `files` on `FeatureSet` (#228)
    * 🚚 Move feature_sets to File
    
    * 🍱 Add data migration
    
    * 💚 Fix
    
    * 💚 Fix

--- lnschema_core/migrations/0009_feature_field_feature_unit_alter_featureset_field_and_more.py
+++ lnschema_core/migrations/0009_feature_field_feature_unit_alter_featureset_field_and_more.py
@@ -50,4 +50,14 @@ class Migration(migrations.Migration):
                 "unique_together": {("feature", "value")},
             },
         ),
+        migrations.AddField(
+            model_name="file",
+            name="feature_sets",
+            field=models.ManyToManyField(related_name="files", to="lnschema_core.featureset"),
+        ),
+        migrations.RunSQL("insert into lnschema_core_file_feature_sets (id, file_id, featureset_id) select id, file_id, featureset_id from lnschema_core_featureset_files"),
+        migrations.RemoveField(
+            model_name="featureset",
+            name="files",
+        ),
     ]
