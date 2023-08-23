8f25d2d8f66de075b6b239a402dc12711601b453
Alex Wolf <f.alexander.wolf@gmail.com>
2023-08-23 07:17:34 +0200

    ♻️ Use CanValidate & HasParents (#277)

--- lnschema_core/__init__.py
+++ lnschema_core/__init__.py
@@ -12,17 +12,17 @@ if _INSTANCE_SETUP:
     from . import ids, types
     from .models import ORM  # backward compat
     from .models import (  # type: ignore
+        CanValidate,
         Dataset,
         Feature,
         FeatureSet,
         File,
+        HasParents,
         Label,
         Modality,
-        ParentsAware,
         Registry,
         Run,
         Storage,
         Transform,
         User,
-        ValidationMixin,
     )
