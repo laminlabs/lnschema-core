953549b3c9d3416c1e8697171a530a5021bd5015
Sunny Sun <38218185+sunnyosun@users.noreply.github.com>
2024-02-27 18:32:59 +0800

    🚚 Use var_field for anndata (#348)
    * 🚚 Use var_field for anndata
    
    * ✏️ Fix typing

--- lnschema_core/models.py
+++ lnschema_core/models.py
@@ -1209,7 +1209,7 @@ class Feature(Registry, CanValidate):
         pass
 
     @classmethod
-    def from_df(cls, df: "pd.DataFrame") -> "RecordsList":
+    def from_df(cls, df: "pd.DataFrame", field: Optional[FieldAttr] = None) -> "RecordsList":
         """Create Feature records for columns."""
         pass
 
