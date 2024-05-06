f293df277d86e187ab9ee49b7f16232dbe8ae19b
Alex Wolf <f.alexander.wolf@gmail.com>
2024-05-06 22:55:24 +0200

    ♻️ Better behavior in current_user_id

--- lnschema_core/users.py
+++ lnschema_core/users.py
@@ -1,27 +1,23 @@
-from lamin_utils import logger
+import lamindb_setup as ln_setup
+from lamindb_setup import settings
+from lamindb_setup._init_instance import register_user
 
 user_id_cache = {}
 
 
 def current_user_id() -> int:
-    # Internal function, do not use directly due to the try & except block
-    # Instead, use lamindb_setup.settings.user.id
-    from lamindb_setup import settings
-
     from lnschema_core.models import User
 
     def query_user_id():
-        try:
-            return User.objects.get(uid=settings.user.uid).id
-        except Exception:
-            logger.warning(
-                "Couldn't retrieve user id (the `created_by` field couldn't be set correctly).\n"
-                "Your user is not yet part of the User registry of this instance. Run\n"
-                "from lamindb_setup._init_instance import register_user\n"
-                "register_user(ln.setup.settings.user)"
-            )
-            # This is needed when first creating the instance during migrations
+        if ln_setup.core.django.IS_MIGRATING:
             return 1
+        else:
+            try:
+                user_id = User.objects.get(uid=settings.user.uid).id
+            except User.DoesNotExist:
+                register_user(settings.user)
+                user_id = User.objects.get(uid=settings.user.uid).id
+            return user_id
 
     if settings._instance_exists:
         if settings.instance.slug not in user_id_cache:
