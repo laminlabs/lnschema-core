79d8f33e8f904a4d3a116864b0e02e7c993af62c
Alex Wolf <f.alexander.wolf@gmail.com>
2024-05-16 17:31:38 +0200

    💚 Fix

--- lnschema_core/migrations/0038_alter_collection_artifact_alter_collection_artifacts_and_more.py
+++ lnschema_core/migrations/0038_alter_collection_artifact_alter_collection_artifacts_and_more.py
@@ -7,7 +7,7 @@ import lnschema_core.users
 
 
 class Migration(migrations.Migration):
-    dependencies = [("lnschema_core", "0001_initial_squashed_0037")]
+    dependencies = [("lnschema_core", "0037_rename_dataset_to_collection")]
 
     operations = [
         migrations.AlterField(
