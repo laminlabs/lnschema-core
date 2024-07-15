e1bb544117c052e3b08d2b7aac5b0e7bce0bd2e3
Alex Wolf <f.alexander.wolf@gmail.com>
2024-07-15 16:27:38 +0200

    💚 Fix

--- noxfile.py
+++ noxfile.py
@@ -14,6 +14,6 @@ def test(session: nox.Session) -> None:
     run(session, "pip install -e .[dev]")
     run(
         session,
-        "pip install lamindb_setup@git+https://github.com/laminlabs/lamindb-setup@cleanupdjango",
+        "pip install lamindb_setup@git+https://github.com/laminlabs/lamindb-setup",
     )
     run_pytest(session, coverage=False)
