02fcafa090283a866c9a61ead5c846b10ecd08ec
Alex Wolf <f.alexander.wolf@gmail.com>
2024-07-31 19:34:35 +0200

    🔥 Remove unnecessary duplication

--- tests/test_integrity.py
+++ tests/test_integrity.py
@@ -2,13 +2,6 @@ import lamindb_setup as ln_setup
 import pytest
 
 
-@pytest.fixture(scope="module")
-def setup_instance():
-    ln_setup.init(storage="./testdb")
-    yield
-    ln_setup.delete("testdb", force=True)
-
-
 def test_migrate_check(setup_instance):
     assert ln_setup.migrate.check()
 
