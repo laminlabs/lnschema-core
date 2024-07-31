62f518cbacceb39e5658649c0381454384d57471
zethson <lukas.heumos@posteo.net>
2024-07-31 14:33:06 +0200

    :art: Test bionty in __repr__
    Signed-off-by: zethson <lukas.heumos@posteo.net>

--- noxfile.py
+++ noxfile.py
@@ -14,6 +14,6 @@ def test(session: nox.Session) -> None:
     run(session, "uv pip install --system -e .[dev]")
     run(
         session,
-        "uv pip install --system lamindb@git+https://github.com/laminlabs/lamindb[bionty]",
+        "uv pip install --system lamindb[bionty]@git+https://github.com/laminlabs/lamindb",
     )
     run_pytest(session, coverage=False)
