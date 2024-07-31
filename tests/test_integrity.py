dccca964939abf40275158d2a1820958d1fc324d
Alex Wolf <f.alexander.wolf@gmail.com>
2024-07-31 19:39:36 +0200

    ➖ Remove bionty & lamindb from dependencies (#408)
    * 🔥 Remove bionty from dependencies
    
    * ✏️ Typo
    
    * 💚 Fix dependency
    
    * 💚 Global fixture
    
    * 🔥 Remove unnecessary duplication
    
    * 💚 Fix

--- tests/test_integrity.py
+++ tests/test_integrity.py
@@ -2,16 +2,9 @@ import lamindb_setup as ln_setup
 import pytest
 
 
-@pytest.fixture(scope="module")
-def setup_bionty_instance():
-    ln_setup.init(storage="./test-bionty-db", schema="bionty")
-    yield
-    ln_setup.delete("test-bionty-db", force=True)
-
-
-def test_migrate_check(setup_bionty_instance):
+def test_migrate_check(setup_instance):
     assert ln_setup.migrate.check()
 
 
-def test_system_check(setup_bionty_instance):
+def test_system_check(setup_instance):
     ln_setup.django("check")
